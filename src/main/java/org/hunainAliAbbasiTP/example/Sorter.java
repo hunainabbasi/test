05c716b677981c767af5e0027d8e992139853b69
hunainabbasi <hunainabbasi94@gmail.com>
2020-07-07 00:30:31 +0500

    tested with big numbers and empty array

--- src/main/java/org/hunainAliAbbasiTP/example/Sorter.java
+++ src/main/java/org/hunainAliAbbasiTP/example/Sorter.java
@@ -9,21 +9,22 @@ public class Sorter
     public static int[] sortOddAndEven(int[] arr) {
 
         int indexCheck = 0;
+        // indexCheck to check number of odd numbers
         for (int num: arr) {
             if (num % 2 != 0)
                 indexCheck++;
         }
 
-        int odd = -1;
-        int even = indexCheck-1;
+        int oddIndex = -1;
+        int evenIndex = indexCheck-1;
         int[] arrayResult = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) { // Even
-                even++;
-                arrayResult[even] = arr[i];
-            } else { // odd
-                odd++;
-                arrayResult[odd] = arr[i];
+            if (arr[i] % 2 == 0) { // if arr[i] is even
+                evenIndex++;
+                arrayResult[evenIndex] = arr[i];
+            } else { // if arr[i] is odd
+                oddIndex++;
+                arrayResult[oddIndex] = arr[i];
             }
         }
 
--- src/test/java/org/hunainAliAbbasiTP/example/SorterTest.java
+++ src/test/java/org/hunainAliAbbasiTP/example/SorterTest.java
@@ -20,25 +20,24 @@ public class SorterTest
         Assert.assertArrayEquals(sorted1, new int[]{1, 3, 7, 2, 4, 16});
 
 
-        // Empty array
         int[] input2 = new int[]{};
         int[] sorted2 = Sorter.sortOddAndEven(input2);
         Assert.assertArrayEquals(sorted2, new int[]{});
 
 
-        int[] input3 = new int[]{1, 2, 4, 6, 3, 7, 16};
+        int[] input3 = new int[]{1 , 2, 4, 8 , 3, 7, 16};
         int[] sorted3 = Sorter.sortOddAndEven(input3);
-        Assert.assertArrayEquals(sorted3, new int[]{1, 3, 7, 2, 4, 6, 16});
+        Assert.assertArrayEquals(sorted3, new int[]{1, 3, 7, 2, 4, 8 ,16});
 
 
-        int[] input4 = new int[]{1, 2, 4, 3, 6, 16};
+        int[] input4 = new int[]{1, 2, 4, 3, 6, 16,100,260};
         int[] sorted4 = Sorter.sortOddAndEven(input4);
-        Assert.assertArrayEquals(sorted4, new int[]{1, 3, 2, 4, 6, 16});
+        Assert.assertArrayEquals(sorted4, new int[]{1, 3, 2, 4, 6, 16,100,260});
 
 
-        int[] input5 = new int[]{2, 4, 6, 8, 1, 3, 5, 7};
+        int[] input5 = new int[]{2, 4, 6, 8, 1, 3, 5, 7,21,31};
         int[] sorted5 = Sorter.sortOddAndEven(input5);
-        Assert.assertArrayEquals(sorted5, new int[]{1, 3, 5, 7, 2, 4, 6, 8});
+        Assert.assertArrayEquals(sorted5, new int[]{1, 3, 5, 7,21,31, 2, 4, 6, 8});
 
     }
 }
